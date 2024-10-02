a805cb1462dd6ef96726d763c93c7c670b60448d
cdufour <opusidea@gmail.com>
2024-10-02 15:31:36 +0200

    init

---
+++ demo/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ChildComponent } from './child/child.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, ChildComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title: string = 'Angular';
+
+  onClick(): undefined {
+    console.log('ok');
+  }
+}
---
+++ demo/src/app/child/child.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-child',
+    template: '<h2>Child</h2>',
+    standalone: true
+})
+export class ChildComponent {
+
+}
