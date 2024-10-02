22f1fcd2b2eb7915981131aa5ca441697e53752a
cdufour <opusidea@gmail.com>
2024-10-02 16:29:51 +0200

    exo1

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChildComponent } from './child/child.component';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChildComponent],
+  imports: [RouterOutlet, ChildComponent, NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title: string = 'Angular';
+  visible: boolean = false;
+  students = ['Fanny', 'Kevin', 'Olivier'];
 
   onClick(): undefined {
     console.log('ok');
--- demo/src/app/child/child.component.ts
+++ demo/src/app/child/child.component.ts
@@ -2,9 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-child',
-    template: '<h2>Child</h2>',
+    template: `
+        <h2>{{ title }} Component</h2>
+    `,
+    //templateUrl: './child.component.html',
     standalone: true
 })
 export class ChildComponent {
-
+    title: string = 'Child';
 }
